6c066f4a3ab7cf91fa422eafde2c25f02d878140
John Flournoy <uoflournoy@gmail.com>
2017-04-28 15:28:49 -0700

    shell fx

---
+++ fMRI/scripts/fx/shell/schedule_spm_jobs/make_sid_matlabbatch.m
@@ -0,0 +1,19 @@
+fileID=fopen(script_file);
+textstuff=textscan(fileID, '%[^\n]');
+fclose(fileID);
+
+regexp=['(''.*)',num2str(replacesid),'(.*'')'];
+replaceexpr=['$1',num2str(sub),'$2'];
+newtext=cellfun(@(x) regexprep(x, regexp, replaceexpr), textstuff{1}, 'UniformOutput', false);
+
+cellfun(@eval, newtext);
+
+[outdir, outfile] = fileparts(script_file);
+
+outdirfull = fullfile(outdir,'sid_batches',outfile);
+
+if ~exist(outdirfull, 'dir')
+  mkdir(outdirfull);
+end
+
+save(fullfile(outdirfull,strcat(num2str(sub),'_',outfile,'.mat')),'matlabbatch');
