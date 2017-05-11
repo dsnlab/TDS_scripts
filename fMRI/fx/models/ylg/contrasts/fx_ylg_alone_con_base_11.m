8ce918f2e6673b2154822332d66b3132a35eee2c
katemills <klmills@uoregon.edu>
2017-05-11 13:18:01 -0700

    added new files

---
+++ fMRI/fx/models/ylg/contrasts/fx_ylg_alone_con_base_11.m
@@ -0,0 +1,40 @@
+%-----------------------------------------------------------------------
+% Job saved on 18-Apr-2017 19:00:04 by cfg_util (rev $Rev: 6460 $)
+% spm SPM - SPM12 (6906)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.con.spmmat = {'/Users/ralph/Documents/tds/fMRI/analysis/fx/models/ylg/fx_ylg_alone/t101/SPM.mat'};
+matlabbatch{1}.spm.stats.con.consess{1}.tcon.name = 'D_A';
+matlabbatch{1}.spm.stats.con.consess{1}.tcon.weights = 1;
+matlabbatch{1}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.name = 'DGvS_A';
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.weights = [0 1];
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{3}.tcon.name = 'DSvG_A';
+matlabbatch{1}.spm.stats.con.consess{3}.tcon.weights = [0 -1];
+matlabbatch{1}.spm.stats.con.consess{3}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{4}.tcon.name = 'O_A';
+matlabbatch{1}.spm.stats.con.consess{4}.tcon.weights = [0 0 0.5 0 0 0 0.5];
+matlabbatch{1}.spm.stats.con.consess{4}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{5}.tcon.name = 'OG_A';
+matlabbatch{1}.spm.stats.con.consess{5}.tcon.weights = [0 0 1];
+matlabbatch{1}.spm.stats.con.consess{5}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{6}.tcon.name = 'GGvGS_A';
+matlabbatch{1}.spm.stats.con.consess{6}.tcon.weights = [0 0 0 1];
+matlabbatch{1}.spm.stats.con.consess{6}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{7}.tcon.name = 'GSvGG_A';
+matlabbatch{1}.spm.stats.con.consess{7}.tcon.weights = [0 0 0 -1];
+matlabbatch{1}.spm.stats.con.consess{7}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{8}.tcon.name = 'OB_A';
+matlabbatch{1}.spm.stats.con.consess{8}.tcon.weights = [0 0 0 0 0 0 1];
+matlabbatch{1}.spm.stats.con.consess{8}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{9}.tcon.name = 'BGvBS_A';
+matlabbatch{1}.spm.stats.con.consess{9}.tcon.weights = [0 0 0 0 0 0 0 1];
+matlabbatch{1}.spm.stats.con.consess{9}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{10}.tcon.name = 'BSvBG_A';
+matlabbatch{1}.spm.stats.con.consess{10}.tcon.weights = [0 0 0 0 0 0 0 -1];
+matlabbatch{1}.spm.stats.con.consess{10}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{11}.tcon.name = 'OGvOB_A';
+matlabbatch{1}.spm.stats.con.consess{11}.tcon.weights = [0 0 1 0 0 0 -1];
+matlabbatch{1}.spm.stats.con.consess{11}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.delete = 1;
---
+++ fMRI/fx/models/ylg/contrasts/load_custom_contrast.m
@@ -0,0 +1,2 @@
+spm('defaults','fMRI')
+load('/Users/ralph/Documents/tds/tds_repo/fMRI/scripts/fx/models/ylg/contrasts/fx_ylg_allconds_con_base_53/fx_ylg_allconds_101.mat')
