cb61c15d078a7ef6865c87c8561596ad6883550d
nandi <nanditav@uoregon.edu>
2017-05-11 16:47:56 -0700

    updated ppc scripts for hpc

--- fMRI/ppc/spm/tds1_tds2/ppc2_tds1_2_job.m
+++ fMRI/ppc/spm/tds1_tds2/ppc2_tds1_2_job.m
@@ -1,6 +1,6 @@
 % List of open inputs
 nrun = X; % enter the number of runs here
-jobfile = {'/Users/ralph/Documents/tds/TDS_scripts/fMRI/ppc/spm/tds1_tds2/ppc1_tds1_2_job.m'};
+jobfile = {'/Users/ralph/Documents/tds/TDS_scripts/fMRI/ppc/spm/tds1_tds2/ppc2_tds1_2_job_job.m'};
 jobs = repmat(jobfile, 1, nrun);
 inputs = cell(0, nrun);
 for crun = 1:nrun
