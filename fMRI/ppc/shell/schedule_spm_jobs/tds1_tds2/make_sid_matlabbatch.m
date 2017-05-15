7e662dcfbae3fe11eba9fa73bffb499b74b832ba
nandi <nanditav@uoregon.edu>
2017-05-15 13:57:44 -0700

    updating ppc2 script tds1_2

--- fMRI/ppc/shell/schedule_spm_jobs/tds1_tds2/make_sid_matlabbatch.m
+++ fMRI/ppc/shell/schedule_spm_jobs/tds1_tds2/make_sid_matlabbatch.m
@@ -1,16 +1,18 @@
 fileID=fopen(script_file);
-textstuff=textscan(fileID, '%[^\n]');
+textstuffcell=textscan(fileID, '%[^\n]');
 fclose(fileID);
 
 regexp=['(''.*)',num2str(replacesid),'(.*'')'];
 replaceexpr=['$1',num2str(sub),'$2'];
-newtext=cellfun(@(x) regexprep(x, regexp, replaceexpr), textstuff{1}, 'UniformOutput', false);
+newtextcell=cellfun(@(x) regexprep(x, regexp, replaceexpr), textstuffcell{1}, 'UniformOutput', false);
 
-cellfun(@eval, newtext);
+newtext=strjoin(newtextcell, '\n');
+
+eval(newtext);
 
 [outdir, outfile] = fileparts(script_file);
 
-outdirfull = fullfile(outdir,'sid_batches');
+outdirfull = fullfile(outdir,'sid_batches',outfile);
 
 if ~exist(outdirfull, 'dir')
   mkdir(outdirfull);
