34aee961d9b7686c0cfab59a6f9d6cd621c41b6b
zopdemac <zopdemac@uoregon.edu>
2017-05-16 13:36:30 -0700

    created new fx model and deleted all old ones

--- fMRI/fx/models/ylg/fx_ylg_alone.m
+++ fMRI/fx/models/ylg/fx_ylg_alone.m
@@ -3,13 +3,13 @@
 % spm SPM - SPM12 (6906)
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
-matlabbatch{1}.cfg_basicio.file_dir.file_ops.file_fplist.dir = {'/Users/ralph/Documents/tds/fMRI/subjects_tds2/t101/stop3'};
+matlabbatch{1}.cfg_basicio.file_dir.file_ops.file_fplist.dir = {'/projects/dsnlab/tds/fMRI/subjects_tds2/101/stop3'};
 matlabbatch{1}.cfg_basicio.file_dir.file_ops.file_fplist.filter = '^swrustop3*';
 matlabbatch{1}.cfg_basicio.file_dir.file_ops.file_fplist.rec = 'FPList';
-matlabbatch{2}.cfg_basicio.file_dir.file_ops.file_fplist.dir = {'/Users/ralph/Documents/tds/fMRI/subjects_tds2/t101/stop4'};
+matlabbatch{2}.cfg_basicio.file_dir.file_ops.file_fplist.dir = {'/projects/dsnlab/tds/fMRI/subjects_tds2/101/stop4'};
 matlabbatch{2}.cfg_basicio.file_dir.file_ops.file_fplist.filter = '^swrustop4*';
 matlabbatch{2}.cfg_basicio.file_dir.file_ops.file_fplist.rec = 'FPList';
-matlabbatch{3}.spm.stats.fmri_spec.dir = {'/Users/ralph/Documents/tds/fMRI/analysis/fx/models/ylg/fx_ylg_alone/t101'};
+matlabbatch{3}.spm.stats.fmri_spec.dir = {'/projects/dsnlab/tds/fMRI/analysis/fx/models/ylg/fx_ylg_alone/101'};
 matlabbatch{3}.spm.stats.fmri_spec.timing.units = 'secs';
 matlabbatch{3}.spm.stats.fmri_spec.timing.RT = 2;
 matlabbatch{3}.spm.stats.fmri_spec.timing.fmri_t = 72;
@@ -17,16 +17,16 @@ matlabbatch{3}.spm.stats.fmri_spec.timing.fmri_t0 = 36;
 matlabbatch{3}.spm.stats.fmri_spec.sess.scans(1) = cfg_dep('File Selector (Batch Mode): Selected Files (^swrustop3*)', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','files'));
 matlabbatch{3}.spm.stats.fmri_spec.sess.scans(2) = cfg_dep('File Selector (Batch Mode): Selected Files (^swrustop4*)', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','files'));
 matlabbatch{3}.spm.stats.fmri_spec.sess.cond = struct('name', {}, 'onset', {}, 'duration', {}, 'tmod', {}, 'pmod', {}, 'orth', {});
-matlabbatch{3}.spm.stats.fmri_spec.sess.multi = {'/Users/ralph/Documents/tds/fMRI/analysis/fx/multicond/ylg/multicond_decout_pmod_t101_stopALONE.mat'};
+matlabbatch{3}.spm.stats.fmri_spec.sess.multi = {'/projects/dsnlab/tds/fMRI/analysis/fx/multicond/ylg/multicond_decout_pmod_101_stopALONE.mat'};
 matlabbatch{3}.spm.stats.fmri_spec.sess.regress = struct('name', {}, 'val', {});
-matlabbatch{3}.spm.stats.fmri_spec.sess.multi_reg = {'/Users/ralph/Documents/tds/fMRI/analysis/fx/motion/auto-motion-output/rp_auto_txt/rp_t101_alone.txt'};
+matlabbatch{3}.spm.stats.fmri_spec.sess.multi_reg = {'/projects/dsnlab/tds/fMRI/analysis/fx/motion/auto-motion-output/rp_auto_txt/rp_101_alone.txt'};
 matlabbatch{3}.spm.stats.fmri_spec.sess.hpf = 128;
 matlabbatch{3}.spm.stats.fmri_spec.fact = struct('name', {}, 'levels', {});
 matlabbatch{3}.spm.stats.fmri_spec.bases.hrf.derivs = [0 0];
 matlabbatch{3}.spm.stats.fmri_spec.volt = 1;
 matlabbatch{3}.spm.stats.fmri_spec.global = 'None';
 matlabbatch{3}.spm.stats.fmri_spec.mthresh = -Inf;
-matlabbatch{3}.spm.stats.fmri_spec.mask = {'/Users/ralph/Documents/tds/fMRI/analysis/masks/tds2/tds_gw_smoothed_group_average_optthresh.nii,1'};
+matlabbatch{3}.spm.stats.fmri_spec.mask = {'/projects/dsnlab/tds/fMRI/analysis/masks/tds2/tds2_gw_smoothed_group_average_optthresh.nii,1'};
 matlabbatch{3}.spm.stats.fmri_spec.cvi = 'AR(1)';
 matlabbatch{4}.spm.stats.fmri_est.spmmat(1) = cfg_dep('fMRI model specification: SPM.mat File', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
 matlabbatch{4}.spm.stats.fmri_est.write_residuals = 0;
