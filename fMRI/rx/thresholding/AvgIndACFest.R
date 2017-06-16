c9d8d5f5705cb8300bec5d158e095931bd8e9e54
Theresa Cheng <tcheng@uoregon.edu>
2017-06-16 11:53:03 -0700

    add indacfest scripts

---
+++ fMRI/rx/thresholding/AvgIndACFest.R
@@ -0,0 +1,26 @@
+
+# Average individual level acf estimates
+# Authors: Theresa W. Cheng, Nandita Vijayakumar
+# Date created; 11/21/2016; Last modified: 6/16/2017
+
+# This script should be run after "acquireACFest.sh". It outputs the mean and median of the individual level acf estimates from second level residuals. The decision to use the mean versus the median is up to the researcher, and might be based on the usual considerations (e.g., examining the distribution and/or presence of outliers). The first three values of either the mean or median are the input for 3dClustSim. 
+
+library(plyr)
+
+# Inputs
+rm(list=ls())
+rx_directory="/Volumes/research/tds/fMRI/analysis/rx/cyb/catchonly_meanCpmod_noquad_w190/indResid_F_2x2_flexi"
+
+# Code
+setwd(rx_directory) 
+logs=list.files(pattern='log*') # list log files
+
+df.logs=lapply(logs, read.table) # read in the list of files
+df.logs=ldply(df.logs, data.frame) # combine the list into one dataframe
+
+df.logs.acf=df.logs[(seq(2,to=nrow(df.logs),by=2)),] #take even rows (aka use acf values and ignore fwhm values)
+
+summarise(df.logs, mean1=mean(df.logs.acf$V1), mean2=mean(df.logs.acf$V2), mean3=mean(df.logs.acf$V3), mean4=mean(df.logs.acf$V4)) #summarise results: mean
+
+summarise(df.logs, median1=median(df.logs.acf$V1), median2=median(df.logs.acf$V2), median3=median(df.logs.acf$V3), mean4=median(df.logs.acf$V4)) #summarise results: median
+
---
+++ fMRI/rx/thresholding/acquireIndACFest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Acquire individual level acf estimates
+# Authors: Theresa W. Cheng, Nandita Vijayakumar
+# Date created; 11/21/2016; Last modified: 6/16/2017
+# This script should be run after writing residuals from second level models in SPM, and before running "AvgIndACFest.R"
+
+export PATH=/usr/local/afni_16.1.06:${PATH} # set path to latest AFNI version
+cd /vxfsvol/home/research/tds/fMRI/analysis/rx/cyb/catchonly_meanCpmod_noquad_w190/indResid_F_2x2_flexi #rx directory w/ residuals
+
+# estimate acf parameters for per subject and save this output to a log file
+for i in Res_* 
+	do 
+	j=${i:0:8}
+	3dFWHMx -acf -mask mask.nii $i >> log_$j.txt
+done
