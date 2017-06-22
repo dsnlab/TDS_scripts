4c64cc80fc80a349039b9e032e0b1cf460ee5706
zdena <zopdemac@uoregon.edu>
2017-06-22 16:42:06 -0700

    added two more contrasts, deleted old

--- fMRI/fx/models/ylg/contrasts/fx_ylg_alone_con_base_13.m
+++ fMRI/fx/models/ylg/contrasts/fx_ylg_alone_con_base_13.m
@@ -1,5 +1,5 @@
 %-----------------------------------------------------------------------
-% Job saved on 18-Apr-2017 19:00:04 by cfg_util (rev $Rev: 6460 $)
+% Job saved on 22-Jun-2017 16:40:22 by cfg_util (rev $Rev: 6460 $)
 % spm SPM - SPM12 (6906)
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
@@ -14,7 +14,7 @@ matlabbatch{1}.spm.stats.con.consess{3}.tcon.name = 'DSvG_A';
 matlabbatch{1}.spm.stats.con.consess{3}.tcon.weights = [0 -1];
 matlabbatch{1}.spm.stats.con.consess{3}.tcon.sessrep = 'none';
 matlabbatch{1}.spm.stats.con.consess{4}.tcon.name = 'O_A';
-matlabbatch{1}.spm.stats.con.consess{4}.tcon.weights = [0 0 0.5 0 0 0 0.5];
+matlabbatch{1}.spm.stats.con.consess{4}.tcon.weights = [0 0 0.5 0 0.5];
 matlabbatch{1}.spm.stats.con.consess{4}.tcon.sessrep = 'none';
 matlabbatch{1}.spm.stats.con.consess{5}.tcon.name = 'OG_A';
 matlabbatch{1}.spm.stats.con.consess{5}.tcon.weights = [0 0 1];
@@ -26,15 +26,21 @@ matlabbatch{1}.spm.stats.con.consess{7}.tcon.name = 'GSvGG_A';
 matlabbatch{1}.spm.stats.con.consess{7}.tcon.weights = [0 0 0 -1];
 matlabbatch{1}.spm.stats.con.consess{7}.tcon.sessrep = 'none';
 matlabbatch{1}.spm.stats.con.consess{8}.tcon.name = 'OB_A';
-matlabbatch{1}.spm.stats.con.consess{8}.tcon.weights = [0 0 0 0 0 0 1];
+matlabbatch{1}.spm.stats.con.consess{8}.tcon.weights = [0 0 0 0 1];
 matlabbatch{1}.spm.stats.con.consess{8}.tcon.sessrep = 'none';
 matlabbatch{1}.spm.stats.con.consess{9}.tcon.name = 'BGvBS_A';
-matlabbatch{1}.spm.stats.con.consess{9}.tcon.weights = [0 0 0 0 0 0 0 1];
+matlabbatch{1}.spm.stats.con.consess{9}.tcon.weights = [0 0 0 0 0 1];
 matlabbatch{1}.spm.stats.con.consess{9}.tcon.sessrep = 'none';
 matlabbatch{1}.spm.stats.con.consess{10}.tcon.name = 'BSvBG_A';
-matlabbatch{1}.spm.stats.con.consess{10}.tcon.weights = [0 0 0 0 0 0 0 -1];
+matlabbatch{1}.spm.stats.con.consess{10}.tcon.weights = [0 0 0 0 0 -1];
 matlabbatch{1}.spm.stats.con.consess{10}.tcon.sessrep = 'none';
 matlabbatch{1}.spm.stats.con.consess{11}.tcon.name = 'OGvOB_A';
-matlabbatch{1}.spm.stats.con.consess{11}.tcon.weights = [0 0 1 0 0 0 -1];
+matlabbatch{1}.spm.stats.con.consess{11}.tcon.weights = [0 0 1 0 -1];
 matlabbatch{1}.spm.stats.con.consess{11}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{12}.tcon.name = 'GGvGS_vs_BGvBS_A';
+matlabbatch{1}.spm.stats.con.consess{12}.tcon.weights = [0 0 0 1 0 -1];
+matlabbatch{1}.spm.stats.con.consess{12}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{13}.tcon.name = 'BGvBS_vs_GGvGS_A';
+matlabbatch{1}.spm.stats.con.consess{13}.tcon.weights = [0 0 0 -1 0 1];
+matlabbatch{1}.spm.stats.con.consess{13}.tcon.sessrep = 'none';
 matlabbatch{1}.spm.stats.con.delete = 1;
