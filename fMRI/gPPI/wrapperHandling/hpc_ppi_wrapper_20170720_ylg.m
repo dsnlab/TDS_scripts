355b7c87b1dc6d4a3e8d82b70245dd4961e26e61
Zdena Op de Macks <zopdemac@uoregon.edu>
2017-07-20 11:39:20 -0700

    updated for use on hpc

--- fMRI/gPPI/wrapperHandling/hpc_ppi_wrapper_20170720_ylg.m
+++ fMRI/gPPI/wrapperHandling/hpc_ppi_wrapper_20170720_ylg.m
@@ -4,25 +4,18 @@ templateSuffix = '_20170720_ylg';
 
 VOIdir = [studyDir 'fMRI/analysis/ppi/ylg/tds2/inputStructs/'];
 
-%User input required (region files)
-regionfile={[VOIdir 'bilat_NAcc_HO_p75.nii']
-    };
-
-%User input required (region names) These are nicknames you can pick.
-region={'bilat_NAcc'};
-
 load([studyDir 'TDS_scripts/fMRI/gPPI/mats/ppi_template' templateSuffix '.mat'])
-P.VOI=regionfile{regionnumber};
-P.Region=region{regionnumber};
-save([studyDir 'fMRI/analysis/ppi/ylg/tds2/inputStructs/' region{regionnumber} '.mat'],'P');
+P.VOI=[VOIdir 'bilat_NAcc_HO_p75.nii'];
+P.Region='bilat_NAcc';
+save([studyDir 'fMRI/analysis/ppi/ylg/tds2/inputStructs/' P.Region '.mat'],'P');
 
         %User input required directory of SPM.mat files
         Directory=[studyDir fxModelFolder '101'];
         cd(Directory)
         %User input required (directory same as line 23 above)
-        load([studyDir 'fMRI/analysis/ppi/ylg/tds2/inputStructs/' region{regionnumber} '.mat'])
+        load([studyDir 'fMRI/analysis/ppi/ylg/tds2/inputStructs/' P.Region '.mat'])
         P.subject='101';
         P.directory=Directory;
         %User input required (change analysis to be more specific)
-        save([studyDir 'fMRI/analysis/ppi/ylg/tds2/' '101' '/' region{regionnumber} '.mat'],'P');
-        PPPI([studyDir 'fMRI/analysis/ppi/ylg/tds2/' '101' '/' region{regionnumber} '.mat']);
+        save([studyDir 'fMRI/analysis/ppi/ylg/tds2/' '101' '/' P.Region '.mat'],'P');
+        PPPI([studyDir 'fMRI/analysis/ppi/ylg/tds2/' '101' '/' P.Region '.mat']);
