f5a202d7f4870c693d7ed8e50f7d07ce29229ec4
Theresa Cheng <tcheng@uoregon.edu>
2017-09-28 18:05:38 -0700

    overwrite conflicted execute_multiple_rx

--- fMRI/rx/models/cyb/execute_multiple_rx.m
+++ fMRI/rx/models/cyb/execute_multiple_rx.m
@@ -2,15 +2,24 @@
 % T. Cheng | Created: 9.27.17 | Last Modified: 9.27.17
 
 cd('/Users/theresacheng/projects/dsnlab/tds/TDS_scripts/fMRI/rx/models/cyb') % directory with rx models
-
 hpc_F_2x2_age
+cd('/Users/theresacheng/projects/dsnlab/tds/TDS_scripts/fMRI/rx/models/cyb') % directory with rx models
 hpc_F_2x2_ageXcontext
+cd('/Users/theresacheng/projects/dsnlab/tds/TDS_scripts/fMRI/rx/models/cyb') % directory with rx models
 hpc_F_2x2_ageXthrow
+cd('/Users/theresacheng/projects/dsnlab/tds/TDS_scripts/fMRI/rx/models/cyb') % directory with rx models
 hpc_F_2x2_agequad
+cd('/Users/theresacheng/projects/dsnlab/tds/TDS_scripts/fMRI/rx/models/cyb') % directory with rx models
 hpc_F_2x2_agequadXcontext
+cd('/Users/theresacheng/projects/dsnlab/tds/TDS_scripts/fMRI/rx/models/cyb') % directory with rx models
 hpc_F_2x2_agequadXthrow
+cd('/Users/theresacheng/projects/dsnlab/tds/TDS_scripts/fMRI/rx/models/cyb') % directory with rx models
 hpc_F_conj_pmod_flexi
+cd('/Users/theresacheng/projects/dsnlab/tds/TDS_scripts/fMRI/rx/models/cyb') % directory with rx models
 hpc_F_conj_pmod_flexi_age
+cd('/Users/theresacheng/projects/dsnlab/tds/TDS_scripts/fMRI/rx/models/cyb') % directory with rx models
 hpc_F_conj_pmod_flexi_ageXpmod
+cd('/Users/theresacheng/projects/dsnlab/tds/TDS_scripts/fMRI/rx/models/cyb') % directory with rx models
 hpc_F_conj_pmod_flexi_agequad
+cd('/Users/theresacheng/projects/dsnlab/tds/TDS_scripts/fMRI/rx/models/cyb') % directory with rx models
 hpc_F_conj_pmod_flexi_agequadXpmod
