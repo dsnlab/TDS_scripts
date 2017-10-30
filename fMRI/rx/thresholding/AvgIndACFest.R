51f8f5c589dda865ff1ccc8a72fd9f0f08592c01
John Flournoy <flournoy@uoregon.edu>
2017-10-30 11:44:51 -0700

    stronger better faster

--- fMRI/rx/thresholding/AvgIndACFest.R
+++ fMRI/rx/thresholding/AvgIndACFest.R
@@ -22,14 +22,23 @@ if (length(args)==0) {
 
 # Code
 setwd(rx_directory) 
-logs=list.files(pattern='log*') # list log files
+logs=list.files(pattern='log.*txt') # list log files
 
 df.logs=lapply(logs, read.table) # read in the list of files
 df.logs=ldply(df.logs, data.frame) # combine the list into one dataframe
 
 df.logs.acf=df.logs[(seq(2,to=nrow(df.logs),by=2)),] #take even rows (aka use acf values and ignore fwhm values)
 
-summarise(df.logs, mean1=mean(df.logs.acf$V1), mean2=mean(df.logs.acf$V2), mean3=mean(df.logs.acf$V3), mean4=mean(df.logs.acf$V4)) #summarise results: mean
 
-summarise(df.logs, median1=median(df.logs.acf$V1), median2=median(df.logs.acf$V2), median3=median(df.logs.acf$V3), median4=median(df.logs.acf$V4)) #summarise results: median
-
+if(length(args==1)){
+  cat(paste(summarise(df.logs,
+                      mean1=mean(df.logs.acf$V1), 
+                      mean2=mean(df.logs.acf$V2), 
+                      mean3=mean(df.logs.acf$V3))[1,1:3],
+            collapse=' ')) #summarise results: mean
+  cat('\n')
+} else {
+  summarise(df.logs, mean1=mean(df.logs.acf$V1), mean2=mean(df.logs.acf$V2), mean3=mean(df.logs.acf$V3), mean4=mean(df.logs.acf$V4)) #summarise results: mean
+  
+  summarise(df.logs, median1=median(df.logs.acf$V1), median2=median(df.logs.acf$V2), median3=median(df.logs.acf$V3), median4=median(df.logs.acf$V4)) #summarise results: median
+}
