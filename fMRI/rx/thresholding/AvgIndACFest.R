458cd5b1d3874fe8f25649d31ea95c7880542026
John Flournoy <flournoy@uoregon.edu>
2017-10-30 09:53:04 -0700

    added ability to run from command line with dir name

--- fMRI/rx/thresholding/AvgIndACFest.R
+++ fMRI/rx/thresholding/AvgIndACFest.R
@@ -7,9 +7,18 @@
 
 library(plyr)
 
+args = commandArgs(trailingOnly=TRUE)
+
 # Inputs
-rm(list=ls())
-rx_directory="/projects/dsnlab/tds/fMRI/analysis/rx/ylg/alone/tds2_n79/regr/cov_puberty/9-bad_go-stop"
+if (length(args)==0) {
+  rm(list=ls())
+  rx_directory="/projects/dsnlab/tds/fMRI/analysis/rx/ylg/alone/tds2_n79/regr/cov_puberty/9-bad_go-stop"
+} else if (length(args)==1) {
+  rx_directory=args[1]
+} else {
+  stop("Single command line argument should be directory")
+}
+
 
 # Code
 setwd(rx_directory) 
@@ -22,5 +31,5 @@ df.logs.acf=df.logs[(seq(2,to=nrow(df.logs),by=2)),] #take even rows (aka use ac
 
 summarise(df.logs, mean1=mean(df.logs.acf$V1), mean2=mean(df.logs.acf$V2), mean3=mean(df.logs.acf$V3), mean4=mean(df.logs.acf$V4)) #summarise results: mean
 
-summarise(df.logs, median1=median(df.logs.acf$V1), median2=median(df.logs.acf$V2), median3=median(df.logs.acf$V3), mean4=median(df.logs.acf$V4)) #summarise results: median
+summarise(df.logs, median1=median(df.logs.acf$V1), median2=median(df.logs.acf$V2), median3=median(df.logs.acf$V3), median4=median(df.logs.acf$V4)) #summarise results: median
 
