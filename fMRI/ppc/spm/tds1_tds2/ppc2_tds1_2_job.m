71f341df1d475fd461ab6c28f5861e98c6552e6d
Nandi <nandi.vij@gmail.com>
2017-11-03 10:32:50 -0700

    re-running ppc1 on 374 and 370

--- fMRI/ppc/spm/tds1_tds2/ppc2_tds1_2_job.m
+++ fMRI/ppc/spm/tds1_tds2/ppc2_tds1_2_job.m
@@ -3,8 +3,8 @@
 % spm SPM - SPM12 (6906)
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
-matlabbatch{1}.spm.tools.dartel.mni_norm.template = {'/projects/dsnlab/shared/tds/fMRI/subjects_tds1_tds2/dartel_template/tds1_2_template_6.nii'};
-matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj.flowfield = {'/projects/dsnlab/shared/tds/fMRI/subjects_tds1_tds2/101/structurals/u_rc1mprage_tds1_2_template.nii'};
+matlabbatch{1}.spm.tools.dartel.mni_norm.template = {'/projects/dsnlab/shared/tds/fMRI/subjects_tds1_tds2/dartel_template/tds1_tds2_template_6.nii'};
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj.flowfield = {'/projects/dsnlab/shared/tds/fMRI/subjects_tds1_tds2/101/structurals/u_rc1mprage_tds1_tds2_template.nii'};
 matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj.images = {'/projects/dsnlab/shared/tds/fMRI/subjects_tds1_tds2/101/structurals/mprage.nii'};
 matlabbatch{1}.spm.tools.dartel.mni_norm.vox = [1 1 1];
 matlabbatch{1}.spm.tools.dartel.mni_norm.bb = [NaN NaN NaN
@@ -15,8 +15,8 @@ matlabbatch{2}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.parent = {'/projects/dsnla
 matlabbatch{2}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.name = 'norm1';
 matlabbatch{3}.cfg_basicio.file_dir.file_ops.file_move.files(1) = cfg_dep('Normalise to MNI Space: MNI Smo. Warped - Concentrations (Deformation 1)', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('{}',{1}, '()',{':'}));
 matlabbatch{3}.cfg_basicio.file_dir.file_ops.file_move.action.moveto(1) = cfg_dep('Make Directory: Make Directory ''norm1''', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','dir'));
-matlabbatch{4}.spm.tools.dartel.mni_norm.template = {'/projects/dsnlab/shared/tds/fMRI/subjects_tds1_tds2/dartel_template/tds1_2_template_6.nii'};
-matlabbatch{4}.spm.tools.dartel.mni_norm.data.subj.flowfield = {'/projects/dsnlab/shared/tds/fMRI/subjects_tds1_tds2/101/structurals/u_rc1mprage_tds1_2_template.nii'};
+matlabbatch{4}.spm.tools.dartel.mni_norm.template = {'/projects/dsnlab/shared/tds/fMRI/subjects_tds1_tds2/dartel_template/tds1_tds2_template_6.nii'};
+matlabbatch{4}.spm.tools.dartel.mni_norm.data.subj.flowfield = {'/projects/dsnlab/shared/tds/fMRI/subjects_tds1_tds2/101/structurals/u_rc1mprage_tds1_tds2_template.nii'};
 matlabbatch{4}.spm.tools.dartel.mni_norm.data.subj.images = {'/projects/dsnlab/shared/tds/fMRI/subjects_tds1_tds2/101/structurals/mprage.nii'};
 matlabbatch{4}.spm.tools.dartel.mni_norm.vox = [2 2 2];
 matlabbatch{4}.spm.tools.dartel.mni_norm.bb = [NaN NaN NaN
@@ -30,8 +30,8 @@ matlabbatch{6}.cfg_basicio.file_dir.file_ops.file_move.action.moveto(1) = cfg_de
 matlabbatch{7}.cfg_basicio.file_dir.file_ops.file_fplist.dir = {'/projects/dsnlab/shared/tds/fMRI/subjects_tds1_tds2/101'};
 matlabbatch{7}.cfg_basicio.file_dir.file_ops.file_fplist.filter = '^ru\w+_[0-9]{4}\.nii';
 matlabbatch{7}.cfg_basicio.file_dir.file_ops.file_fplist.rec = 'FPListRec';
-matlabbatch{8}.spm.tools.dartel.mni_norm.template = {'/projects/dsnlab/shared/tds/fMRI/subjects_tds1_tds2/dartel_template/tds1_2_template_6.nii'};
-matlabbatch{8}.spm.tools.dartel.mni_norm.data.subj.flowfield = {'/projects/dsnlab/shared/tds/fMRI/subjects_tds1_tds2/101/structurals/u_rc1mprage_tds1_2_template.nii'};
+matlabbatch{8}.spm.tools.dartel.mni_norm.template = {'/projects/dsnlab/shared/tds/fMRI/subjects_tds1_tds2/dartel_template/tds1_tds2_template_6.nii'};
+matlabbatch{8}.spm.tools.dartel.mni_norm.data.subj.flowfield = {'/projects/dsnlab/shared/tds/fMRI/subjects_tds1_tds2/101/structurals/u_rc1mprage_tds1_tds2_template.nii'};
 matlabbatch{8}.spm.tools.dartel.mni_norm.data.subj.images(1) = cfg_dep('File Selector (Batch Mode): Selected Files (^ru\w+_[0-9]{4}\.nii)', substruct('.','val', '{}',{7}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','files'));
 matlabbatch{8}.spm.tools.dartel.mni_norm.vox = [2 2 2];
 matlabbatch{8}.spm.tools.dartel.mni_norm.bb = [NaN NaN NaN
