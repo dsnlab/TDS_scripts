bbc2ebd3a646a6e55d38e77f3badd4fbfa464ee9
Nandi Vijayakumar <nandi.vij@gmail.com>
2017-12-04 12:52:06 -0800

    updating DICOM conversion scripts and PPC QC

--- fMRI/ppc/spm/QC/make_ppc_qc_jobs_rawnii.m
+++ fMRI/ppc/spm/QC/make_ppc_qc_jobs_rawnii.m
@@ -2,35 +2,34 @@
 % 5.11.2017
 
 % define paths
-jobDir = '~/Desktop/TDS_QC/';
-%jobDir = '/projects/dsnlab/tds/TDS_scripts/fMRI/ppc/spm/tds1_tds2/QCraw/';
+jobDir = '/projects/dsnlab/tds/TDS_scripts/fMRI/ppc/spm/QC/QC_ppced';
 
 addpath(jobDir)
 
 % loop through subjects and replace subject ID, wave, reo parameters
-for subCount = 300:399;
+for subCount = 300:411;
     subID = (num2str(subCount))
         fn = ['/Volumes/TDS/nonbids_data/fMRI/subjects/',subID];
         if exist(fn, 'dir')
-        matlabbatch{1}.spm.util.checkreg.data = {
-                                                 ['/Volumes/TDS/nonbids_data/fMRI/subjects/',subID,'/structurals/mprage.nii,1']
-                                                 ['/Volumes/TDS/nonbids_data/fMRI/subjects/',subID,'/fmaps/fieldmap.nii,1']
-                                                 ['/Volumes/TDS/nonbids_data/fMRI/subjects/',subID,'/cyb1/cyb1_0001.nii,1']
-                                                 ['/Volumes/TDS/nonbids_data/fMRI/subjects/',subID,'/cyb2/cyb2_0001.nii,1']
-                                                 ['/Volumes/TDS/nonbids_data/fMRI/subjects/',subID,'/stop3/stop3_0001.nii,1']
-                                                 ['/Volumes/TDS/nonbids_data/fMRI/subjects/',subID,'/stop4/stop4_0001.nii,1']
-                                                 ['/Volumes/TDS/nonbids_data/fMRI/subjects/',subID,'/stop5/stop5_0001.nii,1']
-                                                 ['/Volumes/TDS/nonbids_data/fMRI/subjects/',subID,'/stop6/stop6_0001.nii,1']
-                                                 ['/Volumes/TDS/nonbids_data/fMRI/subjects/',subID,'/stop7/stop7_0001.nii,1']
-                                                 ['/Volumes/TDS/nonbids_data/fMRI/subjects/',subID,'/stop8/stop8_0001.nii,1']
-                                                 ['/Volumes/TDS/nonbids_data/fMRI/subjects/',subID,'/vid1/vid1_0001.nii,1']
-                                                 ['/Volumes/TDS/nonbids_data/fMRI/subjects/',subID,'/vid2/vid2_0001.nii,1']
+        matlabbatch{1}.cfg_basicio.file_dir.dir_ops.cfg_cd.dir = {'/Volumes/TDS/nonbids_data/derivatives/fMRI/ppc/QC/QC_ppced'};
+        matlabbatch{2}.spm.util.checkreg.data = {
+                                                 ['/Volumes/TDS/nonbids_data/derivatives/fMRI/ppc/subjects_tdsall/',subID,'/structurals/norm2/swmprage.nii,1']
+                                                 ['/Volumes/TDS/nonbids_data/derivatives/fMRI/ppc/subjects_tdsall/',subID,'/cyb1/swrucyb1_0001.nii,1']
+                                                 ['/Volumes/TDS/nonbids_data/derivatives/fMRI/ppc/subjects_tdsall/',subID,'/cyb2/swrucyb2_0001.nii,1']
+                                                 ['/Volumes/TDS/nonbids_data/derivatives/fMRI/ppc/subjects_tdsall/',subID,'/stop3/swrustop3_0001.nii,1']
+                                                 ['/Volumes/TDS/nonbids_data/derivatives/fMRI/ppc/subjects_tdsall/',subID,'/stop4/swrustop4_0001.nii,1']
+                                                 ['/Volumes/TDS/nonbids_data/derivatives/fMRI/ppc/subjects_tdsall/',subID,'/stop5/swrustop5_0001.nii,1']
+                                                 ['/Volumes/TDS/nonbids_data/derivatives/fMRI/ppc/subjects_tdsall/',subID,'/stop6/swrustop6_0001.nii,1']
+                                                 ['/Volumes/TDS/nonbids_data/derivatives/fMRI/ppc/subjects_tdsall/',subID,'/stop7/swrustop7_0001.nii,1']
+                                                 ['/Volumes/TDS/nonbids_data/derivatives/fMRI/ppc/subjects_tdsall/',subID,'/stop8/swrustop8_0001.nii,1']
+                                                 ['/Volumes/TDS/nonbids_data/derivatives/fMRI/ppc/subjects_tdsall/',subID,'/vid1/swruvid1_0001.nii,1']
+                                                 ['/Volumes/TDS/nonbids_data/derivatives/fMRI/ppc/subjects_tdsall/',subID,'/vid2/swruvid2_0001.nii,1']
                                                  };
-        matlabbatch{2}.spm.util.print.fname = ['rawnii_',subID];
-        matlabbatch{2}.spm.util.print.fig.figname = 'Graphics';
-        matlabbatch{2}.spm.util.print.opts = 'pdf';
+        matlabbatch{3}.spm.util.print.fname = ['ppced_',subID];
+        matlabbatch{3}.spm.util.print.fig.figname = 'Graphics';
+        matlabbatch{3}.spm.util.print.opts = 'pdf';
         
-        saveJob = [jobDir,[subID,'_qc_rawnii.mat']];
+        saveJob = [jobDir,[subID,'_qc_ppced.mat']];
         save(saveJob,'matlabbatch');
         clear matlabbatch; 
         end
