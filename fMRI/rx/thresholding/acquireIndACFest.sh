5e6fc31ff4f468407f48891cdefcaa95d81887a2
Nandi Vijayakumar <nandi.vij@gmail.com>
2018-09-10 11:20:55 -0700

    running acf estimates on rsq models

--- fMRI/rx/thresholding/acquireIndACFest.sh
+++ fMRI/rx/thresholding/acquireIndACFest.sh
@@ -5,10 +5,10 @@
 # Date created; 11/21/2016; Last modified: 6/16/2017
 # This script should be run after writing residuals from second level models in SPM, and before running "AvgIndACFest.R"
 
-rx_path=/projects/dsnlab/tds/fMRI/analysis/rx
+rx_path=/projects/dsnlab/shared/tds/fMRI/analysis/rx
 module load afni
 #export PATH=/usr/local/afni_16.1.06:${PATH} # set path to latest AFNI version
-cd ${rx_path}/cyb/group_comp/flexi_2x2x2 #rx directory w/ residuals
+cd ${rx_path}/cyb/tds2_N69/F_conj_pmod_flexi_rsq_anxious #rx directory w/ residuals
 
 # estimate acf parameters for per subject and save this output to a log file
 for i in Res_* 
