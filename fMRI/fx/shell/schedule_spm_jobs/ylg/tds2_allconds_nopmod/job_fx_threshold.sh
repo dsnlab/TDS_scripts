c986a68fe6af4947d51feafa4029029cee358b8f
jessicaflannery <jflanner@uoregon.edu>
2018-11-21 13:18:29 -0800

    updated thresholding script pt 1

--- fMRI/fx/shell/schedule_spm_jobs/ylg/tds2_allconds_nopmod/job_fx_threshold.sh
+++ fMRI/fx/shell/schedule_spm_jobs/ylg/tds2_allconds_nopmod/job_fx_threshold.sh
@@ -14,17 +14,31 @@ echo "${SUB}"
 echo "Merging residuals"
 echo -------------------------------------------------------------------------------
 
-module load fsl
-script_dir=$(pwd)
-RUNS=$(ls residuals*.txt | wc -l)
-res_dir=/projects/dsnlab/shared/tag/nonbids_data/fMRI/fx/models/dsd/wave1/pmod/MLmotion_FAST_RT/sub-TAG${SUB}
-cd ${res_dir}
+fx_dir=/projects/dsnlab/shared/tag/nonbids_data/fMRI/fx/models/dsd/wave1/pmod/MLmotion_FAST_RT/sub-TAG${SUB}
+sub_dir= (use for loop)
+# N runs for residual calculation
+RUNS=(6)
+STOP=(stop3 stop4 stop5 stop6 stop7 stop8)
+# Make text file with residual files for each run
+for i in $STOP; do 
+	cd $sub_dir/$i
+	num=$(ls -l | grep -v ${i}_0*  | wc -l)
+	echo $(printf "Res_%04d.nii\n" {1..$num}) > $res_dir/residuals_${i}.txt
+done
 
-for i in $(seq 1 $RUNS)
-	do echo "merging residuals for run${i}"
-	residual_files=`cat ${script_dir}/residuals_run${i}.txt`
-	fslmerge -t residuals_run${i} ${residual_files}
-	rm ${residual_files}
+#echo $(printf "Res_%04d.nii\n" {226..450}) > residuals_run2.txt
+
+module load fsl
+#script_dir=$(pwd)
+#RUNS=$(ls residuals*.txt | wc -l)
+
+cd ${fx_dir}
+
+for i in $STOP; do
+	echo "merging residuals for ${i}"
+	residual_files=`cat ${fx_dir}/residuals_${i}.txt`
+	fslmerge -t residuals_${i} ${residual_files}
+	#rm ${residual_files}
 done
 
 # run 3dFWHMx
@@ -33,9 +47,10 @@ echo "Calculating ACF parameters"
 echo -------------------------------------------------------------------------------
 
 module load afni
-for i in $(seq 1 $RUNS)
-	do echo "calculating ACF parameters for run${i}"
-	3dFWHMx -acf -mask mask.nii residuals_run${i}.nii.gz >> ACFparameters.1D
+
+for i in $STOP; do
+	echo "calculating ACF parameters for ${i}"
+	3dFWHMx -acf -mask mask.nii residuals_${i}.nii.gz >> ACFparameters.1D
 done
 
 # average ACF parameters
