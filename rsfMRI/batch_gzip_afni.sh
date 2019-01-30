2d3774ca628b9d58456f25917c5be08250630fc3
Nandi Vijayakumar <nandi.vij@gmail.com>
2019-01-30 13:58:56 -0800

    catching up on commits

--- rsfMRI/batch_gzip_afni.sh
+++ rsfMRI/batch_gzip_afni.sh
@@ -10,9 +10,9 @@ STUDY=/projects/dsnlab/tds
 
 # Set subject list
 SUBJLIST=`cat subject_list.txt`
-#SUBJLIST=`cat test.txt`
+#SUBJLIST=`cat alignment.txt`
 
 for SUBJ in $SUBJLIST
- do sbatch --export SUBID=${SUBJ} --job-name gzipit --partition=defq --mem-per-cpu=8G --cpus-per-task=1 -o "${STUDY}"/TDS_scripts/rsfMRI/output/"${SUBJ}"_gzipit_output.txt -e "${STUDY}"/TDS_scripts/rsfMRI/output/"${SUBJ}"_gzipit_error.txt job_gzip_afni.sh
+ do sbatch --export SUBID=${SUBJ} --job-name gzipit --partition=short --mem-per-cpu=8G --cpus-per-task=1 -o "${STUDY}"/TDS_scripts/rsfMRI/output/"${SUBJ}"_gzipit_output.txt -e "${STUDY}"/TDS_scripts/rsfMRI/output/"${SUBJ}"_gzipit_error.txt job_gzip_afni.sh
 done
 
