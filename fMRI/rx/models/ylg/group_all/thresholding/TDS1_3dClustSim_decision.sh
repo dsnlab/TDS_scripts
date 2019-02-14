d9edbf602729a9457232cc7fa194066dac0b1dce
Jessica <jflanner@uoregon.edu>
2019-02-14 09:30:47 -0800

    wrappin up

--- fMRI/rx/models/ylg/group_all/thresholding/TDS1_3dClustSim_decision.sh
+++ fMRI/rx/models/ylg/group_all/thresholding/TDS1_3dClustSim_decision.sh
@@ -13,10 +13,10 @@ module load prl
 module load afni
 
 # Set path to the RX directory to save output
-rx_path=/projects/dsnlab/shared/tds/fMRI/analysis/rx/ylg/all/tds2/3dMVM_nopmod/
+rx_path=/projects/dsnlab/shared/tds/fMRI/analysis/rx/ylg/all/tds1/Decisions/
 
 # Set path to the mask used in analyses 
-mask=/projects/dsnlab/shared/tds/fMRI/analysis/masks/tds2/tds2_gw_smoothed_group_average_optthr_2mm.nii
+mask=/projects/dsnlab/shared/tds/fMRI/analysis/masks/tds1_tds2/tds1_tds2_gw_smoothed_group_average_optthr_2mm.nii
 
-# Run 3dClustSim using the average acf outputs from the script "2_average_incACF.Rmd"
-3dClustSim -mask $mask -acf 0.73527 4.6591 12.53 > ${rx_path}/threshold.txt
+# Run 3dClustSim using the average acf outputs from the script "calcualte_average_ACF.Rmd"
+3dClustSim -mask $mask -acf 0.670713842105263 4.57508543859649 9.21131210526316 > ${rx_path}/threshold.txt
---
+++ fMRI/rx/models/ylg/group_all/thresholding/TDS1_3dClustSim_outcome_group.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#--------------------------------------------------------------
+#
+#SBATCH --job-name=3dClustSim
+#SBATCH --output=output/3dClustSim.log
+#SBATCH --error=output/3dClustSim_err.log
+#SBATCH --cpus-per-task=25
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=4000
+#SBATCH --partition=fat,short
+
+module load prl
+module load afni
+
+# Set path to the RX directory to save output
+rx_path=/projects/dsnlab/shared/tds/fMRI/analysis/rx/ylg/all/tds1/Outcomes/Group/
+
+# Set path to the mask used in analyses 
+mask=/projects/dsnlab/shared/tds/fMRI/analysis/masks/tds1_tds2/tds1_tds2_gw_smoothed_group_average_optthr_2mm.nii
+
+# Run 3dClustSim using the average acf outputs from the script "calcualte_average_ACF.Rmd"
+3dClustSim -mask $mask -acf 0.670713842105263 4.57508543859649 9.21131210526316 > ${rx_path}/threshold.txt
