fe61dc82641ca234b75a77965484760ca16cc1e4
Theresa Cheng <tcheng@uoregon.edu>
2019-12-01 14:37:33 -0800

    set up template fx script with updated paths

---
+++ fMRI/fx/models/cyb/tds2_quad/fx_tds2_quad_inputs.m
@@ -0,0 +1,9 @@
+% List of open inputs
+nrun = X; % enter the number of runs here
+jobfile = {'/Users/theresacheng/projects/dsnlab/TDS/TDS_scripts/fMRI/fx/models/cyb/tds2_quad/fx_tds2_job_job.m'};
+jobs = repmat(jobfile, 1, nrun);
+inputs = cell(0, nrun);
+for crun = 1:nrun
+end
+spm('defaults', 'FMRI');
+spm_jobman('run', jobs, inputs{:});
